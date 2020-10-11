e8761008b131d75d8f65ca750676fcc788822da8
Anime47 <Anime47@DESKTOP-M6BEAUC>
2020-10-11 20:28:18 +0700

    bai tap 9

---
+++ Example-09/src/main/java/com/leminhduc/service/CustomerService.java
@@ -0,0 +1,35 @@
+package com.leminhduc.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.leminhduc.dao.CustomerDAO;
+import com.leminhduc.entities.Customer;
+@Service
+@Transactional
+public class CustomerService {
+  @Autowired
+  private CustomerDAO customerDAO;
+  
+  public List<Customer> findAll() {
+    return customerDAO.findAll();
+  }
+  public Customer findById(int id) {
+    return customerDAO.findById(id);
+  }
+  
+  public void save(Customer customer){
+    // validate business
+    customerDAO.save(customer);
+  }
+  public void update(Customer customer){
+    // validate business
+    customerDAO.update(customer);
+  }
+  
+  public void delete(int id){
+    // validate business
+    customerDAO.delete(id);
+  }
+}
